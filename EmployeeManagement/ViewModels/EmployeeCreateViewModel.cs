13c27735c49f93f43fd2acc5b3119b04bf553fd0
opic <floresopic@gmail.com>
2019-11-14 17:14:40 +0800

    feature(employee): add upload photo

--- EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
+++ EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
@@ -1,15 +1,15 @@
-﻿using System;
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EmployeeManagement.Models
+namespace EmployeeManagement.ViewModels
 {
-    public class Employee
+    public class EmployeeCreateViewModel
     {
-        public int Id { get; set; }
-
         [Required]
         [MaxLength(10, ErrorMessage = "Name cannot exceed 10 characters")]
         public string Name { get; set; }
@@ -23,6 +23,6 @@ namespace EmployeeManagement.Models
         // ? Dept is nullable to make required field invokable
         public Dept? Department { get; set; }
 
-        public string PhotoPath { get; set; }
+        public IFormFile Photo { get; set; }
     }
 }
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This is an introductionary course in ASP.NET Core MVC. In this course we are bui
 * MVC essentials
 * Dependecy Injection `interfaces and repository pattern`
 * Entity Framework and DbContext Migration
+* File Upload
 
 ### References
 * https://www.youtube.com/watch?v=A5S23KS_-bU&list=PLEhEHUEU3x5q-xB1On4CsLPts0-rZ9oos
